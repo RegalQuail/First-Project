38eff81c4bdd42c4e298f64178196e97777beef0
RegalQuail <82899321+RegalQuail@users.noreply.github.com>
2021-05-30 12:37:15 +0200

    more JS

--- index.js
+++ index.js
@@ -2,3 +2,27 @@ var first = document.getElementById('first');
 console.log(first);
 
 var input = document.getElementById('userInput');
+var enterButton = document.getElementById('enter');
+var item = document.getElementsByTagName('li');
+var ul = document.querySelector('ul');
+
+function inputLength() {
+    return input.value.length;
+}
+
+function listLength() {
+    return item.length;
+}
+
+function createListElement() {
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+}
+
+function addListAfterClick() {
+    if (inputLength() > 0) {
+        createListElement();
+    }
+}
